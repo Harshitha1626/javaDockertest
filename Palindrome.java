6376dd615adffe328d9f32caebcb3e380d86221a
Harshitha <sindhuharshitha7@gmail.com>
2024-12-16 09:05:40 +0530

    java Docker

---
+++ Palindrome.java
@@ -0,0 +1,10 @@
+import util.java.Scanner;
+public class Palindrome{
+	public static void main(String[] ardgs){
+		Scanner sc = new Scanner(System.in);
+		int num = nextInt(),rev=0,temp=num;
+		while(num>0)rev=rev*10+num%10,num/10;
+		System.out.println(temp==rev?"palindrome:" "Not palindrome:");
+		sc.close()
+	}
+}
