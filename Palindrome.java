9a33975570571812d0946d12ef2943f90cd010e1
Harshitha <sindhuharshitha7@gmail.com>
2024-12-16 09:16:29 +0530

    java Docker

--- Palindrome.java
+++ Palindrome.java
@@ -3,8 +3,8 @@ public class Palindrome{
 	public static void main(String[] ardgs){
 		Scanner sc = new Scanner(System.in);
 		int num = nextInt(),rev=0,temp=num;
-		while(num>0)rev=rev*10+num%10,num/10;
-		System.out.println(temp==rev?"palindrome:" "Not palindrome:");
+		while(num>0)rev=rev*10+num%10,num/10=0;
+		System.out.println(temp==rev?"palindrome": "Not palindrome":);
 		sc.close();
 	}
 }
